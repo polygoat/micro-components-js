4778ead391b14891fe832975d29d2d247c01c040
Dan Borufka <dan@polygoat.org>
2020-11-21 23:09:51 +0100

    Cleaning up Builder component

--- components/builder.js
+++ components/builder.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
 
+require('colors');
 const fs = require('fs');
 const _ = require('lodash');
 const path = require('path');
-const { Config } = require('../../utils/classes/Config');
 
-Config.set_project_root(process.cwd());
-const CONFIG = new Config('/config/micro-pipeline.json').load();
-
-const { Component } = require('../../utils/classes/Component');
-const { parse_variables } = require('../../utils/templates');
-const { TERMINAL_FORMATS } = require('../../utils/console');
+const { Component } = require('../utils/classes/Component');
 
 const MAPPING = {
 	'js': 'node',
@@ -27,14 +22,14 @@ const Builder = new Component({
 	name: 'builder',
 
 	create(entity, service_name, coding_language='node') {
-		data = {
+		const data = {
 			name: _.snakeCase(service_name),
 			class_name: _.startCase(_.camelCase(service_name)).replace(/ /g, ''),
 			cwd: process.cwd()
 		}
-		coding_language = _.get(MAPPING, coding_language, coding_language)
+		coding_language = _.get(MAPPING, coding_language, coding_language);
 
-		const render = _.template(fs.readFileSync(`./${CONFIG.PATHS.SERVICES}/blueprints/${coding_language}.${entity}`).toString());
+		const render = _.template(fs.readFileSync(`./${coding_language}.${entity}`).toString());
 		let file = render(data);
 
 		const file_ending = ENDINGS[coding_language];
@@ -48,6 +43,8 @@ const Builder = new Component({
 		fs.writeFileSync(file_path, file);
 		fs.chmodSync(file_path, '755');
 
-		console.log(`Component created. Try it by running ${TERMINAL_FORMATS.WARNING}./${file_path} help${TERMINAL_FORMATS.RESET}`);
+		console.log(' Component created. Try it by running ' + './${file_path} help'.yellow);
 	}
 });
+
+Builder.export_as_cli();
